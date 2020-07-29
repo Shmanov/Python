08a9a7d21afd07f27b47f85ed09ba8e18af1106e
IgorShmanov <i.v.shmanov@mail.ru>
2020-07-29 15:54:18 +0400

    Выполнены задания к 6 уроку

--- Lessons/lesson_5/hw_3.py
+++ Lessons/lesson_5/hw_3.py
@@ -4,14 +4,16 @@
 # Выполнить подсчет средней величины дохода сотрудников
 
 
-try:
-    with open("Staff.txt", 'r') as f_obj:
+#try:
+#    with open("Staff.txt", 'r') as f_obj:
                 
+#        text = f_obj.readlines()
+#        #print(text)
+#        print("Сотрудники с окладом меньше 20000:" + '\n')
+#        for i in text:
+#            pers = i.split(' ')
+#            if int(pers[1]) < 20000:
+#                print(pers[0] , pers[1])
         
-        text = f_obj.readlines()
-        print(text)
-        
-        
-        
-except IOError:
-    print("Произошла ошибка ввода-вывода!")
+#except IOError:
+#    print("Произошла ошибка ввода-вывода!")
